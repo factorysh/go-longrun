61f59ffbd98617f45925bde5629e457272cb4d69
Mathieu Lecarme <mlecarme@bearstech.com>
2020-03-06 21:38:53 +0100

    POST + event stream.

--- rest/rest.go
+++ rest/rest.go
@@ -1,6 +1,7 @@
 package rest
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -24,12 +25,24 @@ func getId(root, path string) (uuid.UUID, error) {
 	return uuid.Parse(path[len(root)+1 : len(path)])
 }
 
+func parseAcceptContains(accept, contains string) bool {
+	for _, slug := range strings.Split(accept, ",") {
+		slug = strings.TrimSpace(slug)
+		n := strings.Split(slug, ";")
+		fmt.Println(n, contains)
+		if n[0] == contains {
+			return true
+		}
+	}
+	return false
+}
+
 func (h *Handler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodHead:
 		id, err := getId(h.root, req.RequestURI)
 		if err != nil {
-			resp.WriteHeader(500)
+			resp.WriteHeader(400)
 			return
 		}
 		_, ok := h.runs.GetRun(id)
@@ -39,12 +52,52 @@ func (h *Handler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 			resp.WriteHeader(404)
 		}
 	case http.MethodPost:
+		if req.RequestURI != h.root+"/" {
+			resp.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
 		b, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			fmt.Println(err)
+			resp.WriteHeader(500)
+		}
+		req.Body.Close()
 		raw := new(json.RawMessage)
 		err = json.Unmarshal(b, raw)
-		fmt.Println(err)
-	case http.MethodGet:
+		if err != nil {
+			fmt.Println(err)
+			resp.WriteHeader(500)
+		}
+		run := h.runs.New()
+		resp.Header().Set("content-type", "application/json")
+		r := bytes.NewBufferString(`{"id":"`)
+		r.WriteString(run.Id().String())
+		r.WriteString(`"}`)
 
+		fmt.Println(req.Header)
+		if parseAcceptContains(req.Header.Get("accept"), "text/event-stream") {
+			fmt.Println("Event-stream rulez")
+			resp.Header().Add("Location", h.root+"/"+run.Id().String())
+			resp.WriteHeader(303)
+			resp.Write(r.Bytes())
+		} else {
+			resp.WriteHeader(201)
+			resp.Write(r.Bytes())
+		}
+	case http.MethodGet:
+		// FIXME
+		fmt.Println("Get Headers", req.Header)
+		id, err := getId(h.root, req.RequestURI)
+		if err != nil {
+			resp.WriteHeader(400)
+			return
+		}
+		_, ok := h.runs.GetRun(id)
+		if ok {
+			resp.WriteHeader(200)
+		} else {
+			resp.WriteHeader(404)
+		}
 	default:
 		resp.WriteHeader(http.StatusMethodNotAllowed)
 	}
--- rest/rest_test.go
+++ rest/rest_test.go
@@ -1,6 +1,10 @@
 package rest
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"testing"
 	"time"
 
@@ -34,4 +38,29 @@ func TestUrl(t *testing.T) {
 	resp, err = client.Head(server.URL + "/user/7D5E3597-9B8C-41A3-AB1C-DA6EAC94A7B8")
 	assert.NoError(t, err)
 	assert.Equal(t, 404, resp.StatusCode)
+
+	req, err := http.NewRequest("POST", server.URL+"/user/", bytes.NewBuffer([]byte(`{"name": "Bob"}`)))
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, 201, resp.StatusCode)
+
+	req, err = http.NewRequest("POST", server.URL+"/user/", bytes.NewBuffer([]byte(`{"name": "Charly"}`)))
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("accept", "text/event-stream")
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+	resp, err = client.Do(req)
+	assert.NoError(t, err)
+	body, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	resp.Body.Close()
+	rjson := make(map[string]string)
+	json.Unmarshal(body, &rjson)
+	fmt.Println("Body: ", string(body))
+	assert.Equal(t, 303, resp.StatusCode)
+	assert.Equal(t, "/user/"+rjson["id"], resp.Header.Get("location"))
 }
